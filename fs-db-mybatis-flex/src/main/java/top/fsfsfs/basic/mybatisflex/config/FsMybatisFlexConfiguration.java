dfae3aad10d9849d0f3cf4e40b5a2eb9bb3bb1de
zuihou <244387066@qq.com>
2024-06-10 22:30:32 +0800

    feat: flex初始配置统一通过 MyBatisFlexCustomizer 回调

--- fs-db-mybatis-flex/src/main/java/top/fsfsfs/basic/mybatisflex/config/FsMybatisFlexConfiguration.java
+++ fs-db-mybatis-flex/src/main/java/top/fsfsfs/basic/mybatisflex/config/FsMybatisFlexConfiguration.java
@@ -1,27 +1,17 @@
 package top.fsfsfs.basic.mybatisflex.config;
 
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.ReflectUtil;
-import com.baidu.fsg.uid.UidGenerator;
-import com.baidu.fsg.uid.buffer.RejectedPutBufferHandler;
-import com.baidu.fsg.uid.buffer.RejectedTakeBufferHandler;
-import com.baidu.fsg.uid.impl.CachedUidGenerator;
-import com.baidu.fsg.uid.impl.DefaultUidGenerator;
-import com.baidu.fsg.uid.impl.HuToolUidGenerator;
-import com.baidu.fsg.uid.worker.DisposableWorkerIdAssigner;
 import com.mybatisflex.core.FlexGlobalConfig;
 import com.mybatisflex.core.audit.AuditManager;
 import com.mybatisflex.core.audit.ConsoleMessageCollector;
 import com.mybatisflex.core.audit.CountableMessageCollector;
 import com.mybatisflex.core.audit.ScheduledMessageCollector;
 import com.mybatisflex.core.keygen.KeyGeneratorFactory;
+import com.mybatisflex.spring.boot.MyBatisFlexCustomizer;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import top.fsfsfs.basic.db.config.FsDbConfiguration;
 import top.fsfsfs.basic.db.properties.DatabaseProperties;
 import top.fsfsfs.basic.mybatisflex.keygen.UidKeyGenerator;
-import top.fsfsfs.basic.uid.dao.WorkerNodeDao;
 
 /**
  * Mybatis flex 常用重用拦截器
@@ -30,52 +20,16 @@ import top.fsfsfs.basic.uid.dao.WorkerNodeDao;
  * @since 2018/10/24
  */
 @Slf4j
-public abstract class FsMybatisFlexConfiguration extends FsDbConfiguration {
+public abstract class FsMybatisFlexConfiguration extends FsDbConfiguration implements MyBatisFlexCustomizer {
     public FsMybatisFlexConfiguration(final DatabaseProperties databaseProperties) {
         super(databaseProperties);
-        audit(databaseProperties);
-
-        KeyGeneratorFactory.register("uid", new UidKeyGenerator());
     }
 
-//    @Override
-//    @Bean
-//    public UidGenerator getHuToolUidGenerator(WorkerNodeDao workerNodeDao) {
-//        switch (databaseProperties.getIdType()) {
-//            case CACHE -> {
-//                DisposableWorkerIdAssigner disposableWorkerIdAssigner = new DisposableWorkerIdAssigner(workerNodeDao);
-//                CachedUidGenerator uidGenerator = new CachedUidGenerator();
-//                DatabaseProperties.CacheId cacheId = databaseProperties.getCacheId();
-//                BeanUtil.copyProperties(cacheId, uidGenerator);
-//                if (cacheId.getRejectedPutBufferHandlerClass() != null) {
-//                    RejectedPutBufferHandler rejectedPutBufferHandler = ReflectUtil.newInstance(cacheId.getRejectedPutBufferHandlerClass());
-//                    uidGenerator.setRejectedPutBufferHandler(rejectedPutBufferHandler);
-//                }
-//                if (cacheId.getRejectedTakeBufferHandlerClass() != null) {
-//                    RejectedTakeBufferHandler rejectedTakeBufferHandler = ReflectUtil.newInstance(cacheId.getRejectedTakeBufferHandlerClass());
-//                    uidGenerator.setRejectedTakeBufferHandler(rejectedTakeBufferHandler);
-//                }
-//                uidGenerator.setWorkerIdAssigner(disposableWorkerIdAssigner);
-//
-//                return uidGenerator;
-//            }
-//            case HU_TOOL -> {
-//                DatabaseProperties.HutoolId id = databaseProperties.getHutoolId();
-//                HuToolUidGenerator uidGenerator = new HuToolUidGenerator(id.getWorkerId(), id.getDataCenterId());
-////                KeyGeneratorFactory.register("uid", new UidKeyGenerator(uidGenerator));
-//                return uidGenerator;
-//            }
-//            default -> {
-//                DisposableWorkerIdAssigner disposableWorkerIdAssigner = new DisposableWorkerIdAssigner(workerNodeDao);
-//                DefaultUidGenerator uidGenerator = new DefaultUidGenerator();
-//                BeanUtil.copyProperties(databaseProperties.getDefaultId(), uidGenerator);
-//                uidGenerator.setWorkerIdAssigner(disposableWorkerIdAssigner);
-////                KeyGeneratorFactory.register("uid", new UidKeyGenerator(uidGenerator));
-//                return uidGenerator;
-//            }
-//        }
-//    }
-
+    @Override
+    public void customize(FlexGlobalConfig globalConfig) {
+        audit(databaseProperties);
+        KeyGeneratorFactory.register("uid", new UidKeyGenerator());
+    }
 
     public static void audit(DatabaseProperties databaseProperties) {
         DatabaseProperties.Flex flex = databaseProperties.getFlex();
