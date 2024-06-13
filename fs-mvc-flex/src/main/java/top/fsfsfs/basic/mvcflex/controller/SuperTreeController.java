7914da8163aff9ecdc34c4e445d942522d43ce7e
zuihou <244387066@qq.com>
2024-06-14 00:54:55 +0800

    feat: 增加Tree结构方法

---
+++ fs-mvc-flex/src/main/java/top/fsfsfs/basic/mvcflex/controller/SuperTreeController.java
@@ -0,0 +1,23 @@
+package top.fsfsfs.basic.mvcflex.controller;
+
+import cn.hutool.core.lang.tree.TreeNode;
+import top.fsfsfs.basic.base.entity.SuperEntity;
+import top.fsfsfs.basic.mvcflex.service.SuperCacheService;
+
+import java.io.Serializable;
+
+/**
+ * SuperCacheController
+ * <p>
+ * 继承该类，在SuperController类的基础上扩展了以下方法：
+ * 1，get ： 根据ID查询缓存，若缓存不存在，则查询DB
+ *
+ * @author tangyh
+ * @since 2020年03月06日11:06:46
+ */
+public abstract class SuperTreeController<S extends SuperCacheService<Entity>,
+        Id extends Serializable, Entity extends SuperEntity<Id>, VO, QueryVO, ResultVO extends TreeNode<Id>>
+        extends SuperController<S, Id, Entity, VO, QueryVO, ResultVO>
+        implements TreeController<Id, Entity, QueryVO, ResultVO> {
+
+}
---
+++ fs-mvc-flex/src/main/java/top/fsfsfs/basic/mvcflex/controller/TreeController.java
@@ -0,0 +1,48 @@
+package top.fsfsfs.basic.mvcflex.controller;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.lang.tree.TreeNode;
+import com.mybatisflex.core.query.QueryWrapper;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import top.fsfsfs.basic.annotation.log.WebLog;
+import top.fsfsfs.basic.base.R;
+import top.fsfsfs.basic.mvcflex.utils.ControllerUtil;
+import top.fsfsfs.util.utils.BeanPlusUtil;
+import top.fsfsfs.util.utils.FsTreeUtil;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 新增
+ *
+ * @param <Entity> 实体
+ * @param <QueryVO> 参数
+ * @param <ResultVO> 返回值
+ * @author tangyh
+ * @since 2020年03月07日22:07:31
+ */
+public interface TreeController<Id extends Serializable, Entity, QueryVO, ResultVO extends TreeNode<Id>>
+        extends PageController<Entity, QueryVO, ResultVO> {
+    /**
+     * 按树结构查询
+     *
+     * @param pageQuery 查询参数
+     * @return
+     */
+    @Operation(summary = "按树结构查询")
+    @PostMapping("/tree")
+    @WebLog("按树结构查询")
+    default R<List<Tree<Id>>> tree(@RequestBody QueryVO pageQuery) {
+        Entity entity = BeanPlusUtil.toBean(pageQuery, getEntityClass());
+        QueryWrapper wrapper = QueryWrapper.create(entity, ControllerUtil.buildOperators(entity.getClass()));
+
+        List<Entity> list = getSuperService().list(wrapper);
+
+        List<ResultVO> treeList = BeanUtil.copyToList(list, getResultVoClass());
+        return success(FsTreeUtil.build(treeList, (Id) FsTreeUtil.DEF_PARENT_ID));
+    }
+}
